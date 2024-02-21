07b49d700334a180e8554df423c5a88b9f45a70e
Sung Gon Kim <sunggon82.kim@lge.com>
2024-02-21 09:26:36 +0900

    Update tests

--- main_test.py
+++ main_test.py
@@ -5,9 +5,23 @@ Copyright (c) 2024 LG Electronics Inc.
 SPDX-License-Identifier: MIT
 """
 
+import os
 import pytest
 import shutil
 import subprocess
+
+def execute(command):
+    return subprocess.run(command, capture_output=True, shell=True, encoding="UTF-8")
+
+def test_testing_disabled(request, tmpdir_factory):
+    directory = str(tmpdir_factory.mktemp("build"))
+    def cleanup():
+        shutil.rmtree(directory)
+    request.addfinalizer(cleanup)
+    execute(f"cmake -S . -B {directory} -DWITH_TEST=OFF").check_returncode()
+    assert "make: *** No rule to make target 'build-test'" in execute(f"cmake --build {directory} --target build-test").stderr
+    assert "make: *** No rule to make target 'test'" in execute(f"cmake --build {directory} --target test").stderr
+    assert not os.path.exists(f"{directory}/CMakeFiles/3.16.3/CMakeHOSTCCompiler.cmake")
 
 @pytest.fixture(scope="session")
 def build_dir(request, tmpdir_factory):
@@ -15,8 +29,8 @@ def build_dir(request, tmpdir_factory):
     def cleanup():
         shutil.rmtree(directory)
     request.addfinalizer(cleanup)
-    subprocess.check_call(f"cmake -S . -B {directory}", shell=True)
-    subprocess.check_call(f"cmake --build {directory} --target build-test", shell=True)
+    execute(f"cmake -S . -B {directory} -DWITH_TEST=ON").check_returncode()
+    execute(f"cmake --build {directory} --target build-test").check_returncode()
     return directory
 
 @pytest.fixture(scope="session")
@@ -24,15 +38,12 @@ def compiler_info(build_dir):
     with open(f"{build_dir}/CMakeFiles/3.16.3/CMakeHOSTCCompiler.cmake", "r") as f:
         return f.read()
 
-
 def test_ctest(build_dir):
-    output = subprocess.check_output(f"cmake --build {build_dir} --target test", shell=True, encoding="UTF-8")
-    assert "unittest_unittest ................   Passed" in output
+    assert "unittest_unittest ................   Passed" in execute(f"cmake --build {build_dir} --target test").stdout
 
 def test_gcovr(build_dir):
-    subprocess.check_call(f"cmake --build {build_dir} --target test", shell=True)
-    output = subprocess.check_output(f"gcovr .", shell=True, encoding="UTF-8")
-    assert "src/test_main.c                               19      19   100%" in output
+    execute(f"cmake --build {build_dir} --target test").check_returncode()
+    assert "src/test_main.c                               19      19   100%" in execute(f"gcovr .").stdout
 
 def test_compiler_info(compiler_info):
     assert 'set(CMAKE_HOSTC_COMPILER "/usr/bin/cc")' in compiler_info
