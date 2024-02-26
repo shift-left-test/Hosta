a3edfeefc01c43240ff8aec5f108835bd4c56b7b
Sung Gon Kim <sunggon82.kim@lge.com>
2024-02-26 14:22:16 +0900

    Update tests

--- main_test.py
+++ main_test.py
@@ -19,35 +19,35 @@ def cmake_fixture(request, tmpdir_factory):
     def cleanup():
         shutil.rmtree(directory)
     request.addfinalizer(cleanup)
-    def _build_dir(testing_enabled=True, generator="Unix Makefiles"):
+    def _execute_fixture(testing_enabled=True, generator="Unix Makefiles"):
         WITH_TEST="ON" if testing_enabled else "OFF"
         execute(f'cmake -S . -B {directory} -DWITH_TEST={WITH_TEST} -G "{generator}"').check_returncode()
-        return directory
-    return _build_dir
+        return lambda target: execute(f"cmake --build {directory} --target {target}"), directory
+    return _execute_fixture
 
 def test_disabled(cmake_fixture):
-    directory = cmake_fixture(testing_enabled=False)
-    assert "make: *** No rule to make target 'build-test'" in execute(f"cmake --build {directory} --target build-test").stderr
-    assert "make: *** No rule to make target 'test'" in execute(f"cmake --build {directory} --target test").stderr
+    run, directory = cmake_fixture(testing_enabled=False)
+    assert "make: *** No rule to make target 'build-test'" in run("build-test").stderr
+    assert "make: *** No rule to make target 'test'" in run("test").stderr
     assert not os.path.exists(f"{directory}/CMakeFiles/3.16.3/CMakeHOSTCCompiler.cmake")
 
 def test_build_test_target(cmake_fixture):
-    directory = cmake_fixture()
-    execute(f"cmake --build {directory} --target build-test").check_returncode()
+    run, _ = cmake_fixture()
+    run("build-test").check_returncode()
 
 def test_ctest(cmake_fixture):
-    directory = cmake_fixture()
-    execute(f"cmake --build {directory} --target build-test")
-    assert "unittest_unittest ................   Passed" in execute(f"cmake --build {directory} --target test").stdout
+    run, _ = cmake_fixture()
+    run("build-test").check_returncode()
+    assert "unittest_unittest ................   Passed" in run("test").stdout
 
 def test_gcovr(cmake_fixture):
-    directory = cmake_fixture()
-    execute(f"cmake --build {directory} --target build-test")
-    execute(f"cmake --build {directory} --target test").check_returncode()
+    run, _ = cmake_fixture()
+    run("build-test").check_returncode()
+    run("test").check_returncode()
     assert "sample/test/test_main.c                       15      15   100%" in execute(f"gcovr .").stdout
 
 def test_compiler_info(cmake_fixture):
-    directory = cmake_fixture()
+    _, directory = cmake_fixture()
     with open(f"{directory}/CMakeFiles/3.16.3/CMakeHOSTCCompiler.cmake", "r") as f:
         compiler_info = f.read()
         assert 'set(CMAKE_HOSTC_COMPILER "/usr/bin/cc")' in compiler_info
