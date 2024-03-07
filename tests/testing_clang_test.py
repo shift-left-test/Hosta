2d857c896d99785d043e50a2573ce7b183bfaf3b
Sung Gon Kim <sunggon82.kim@lge.com>
2024-03-07 10:41:50 +0900

    Fix a test failure

--- tests/testing_clang_test.py
+++ tests/testing_clang_test.py
@@ -13,7 +13,7 @@ def test_host_compiler_info(testing_clang):
     assert 'set(CMAKE_HOSTC_COMPILER_WORKS TRUE)' in compiler_info
     assert 'set(CMAKE_HOSTC_ABI_COMPILED TRUE)' in compiler_info
     assert 'set(CMAKE_HOSTC_COMPILER_ABI "ELF")' in compiler_info
-    assert 'set(CMAKE_HOSTC_IMPLICIT_INCLUDE_DIRECTORIES "/usr/local/include;/usr/lib/llvm-10/lib/clang/10.0.1/include;/usr/include/x86_64-linux-gnu;/usr/include")' in compiler_info
+    assert 'set(CMAKE_HOSTC_IMPLICIT_INCLUDE_DIRECTORIES "/usr/local/include;/usr/lib/llvm-10/lib/clang/10.0.0/include;/usr/include/x86_64-linux-gnu;/usr/include")' in compiler_info
     assert 'set(CMAKE_HOSTC_IMPLICIT_LINK_LIBRARIES "gcc;gcc_s;c;gcc;gcc_s")' in compiler_info
     assert 'set(CMAKE_HOSTC_IMPLICIT_LINK_DIRECTORIES "/usr/lib/gcc/x86_64-linux-gnu/9;/usr/lib/x86_64-linux-gnu;/usr/lib64;/lib/x86_64-linux-gnu;/lib64;/usr/lib;/usr/lib/llvm-10/lib;/lib")' in compiler_info
     assert 'set(CMAKE_HOSTC_IMPLICIT_LINK_FRAMEWORK_DIRECTORIES "")' in compiler_info
